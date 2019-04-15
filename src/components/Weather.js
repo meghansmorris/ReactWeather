804ad469238d7b36f71924acd150f4a28d2d9572
itsmegh <42684917+itsmegh@users.noreply.github.com>
2019-04-15 15:00:47 -0600

    finalized basic application, not working because need to properly integrated hidden API key

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,11 +1,17 @@
 import React from "react";
 
-class Weather extends React.Component {
-    render() {
-        return (
-            <div>Weather Component</div>
-        )
-    }
+
+const Weather = (props) => {
+    return (
+        <>
+            {props.icon && <img src={"http://openweathermap.org/img/w/" + props.icon + ".png"} alt="weather icon"></img>}
+            {props.city && props.country && <h2>Location: { props.city }, { props.country }</h2>}
+            {props.desc && <p>Current Weather Conditions: { props.desc }</p>}
+            {props.temp && <h3>Current Temperature: { props.temp } F</h3>}        
+            {props.humidity && <h5>Current Humidity: { props.humidity } %</h5>}
+            {props.error && <p>{ props.error }</p>}              
+        </>
+    )
 };
 
 export default Weather;
